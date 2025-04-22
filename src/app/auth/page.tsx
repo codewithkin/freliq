7b6d4123899bc8aec36a33499659c048a2d13a21
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-22 14:58:25 +0200

    feat: create Auth component with welcome message and layout

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,18 @@
+import { Card } from "@/components/ui/card";
+
+function Auth() {
+  return (
+    <section className="min-h-screen w-full">
+      <Card className="w-fit h-fit px-8">
+        <article>
+          <h2 className="text-2xl font-semibold">Welcome to Freliq</h2>
+          <p className="text-muted-foreground text-sm">
+            Transparency. Trust. Teamwork. Delivered daily.
+          </p>
+        </article>
+      </Card>
+    </section>
+  );
+}
+
+export default Auth;
