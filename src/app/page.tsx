dd1d899099a1d844362832baa6cad160cabbedd2
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-22 14:46:43 +0200

    feat: implement user session handling and redirection in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
+import { authClient } from "@/lib/auth-client";
+import { Loader2 } from "lucide-react";
+import { redirect } from "next/navigation";
+
 function Home() {
-  return <div></div>;
+  // Make sure the user is logged in
+  const {
+    data: session,
+    isPending, //loading state
+  } = authClient.useSession();
+
+  if (isPending) {
+    return (
+      <section className="min-h-screen min-w-screen">
+        <article className="flex flex-col gap-2 items-center justify-center">
+          <Loader2 className="animate-spin text-primary" size={40} />
+        </article>
+      </section>
+    );
+  }
+
+  // If the user is not logged in...
+  if (!session) {
+    // ...redirect to the auth page
+    redirect("/auth");
+  }
+
+  // Otherwise...
+  // ...redirect to the dashboard page
+  redirect("/dashboard");
 }
 
 export default Home;
