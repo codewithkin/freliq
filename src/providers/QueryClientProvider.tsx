b7b6b5d00c65f18e06d74aa1b6e0cb7945466053
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-24 12:10:05 +0200

    feat: add Syncfusion dependencies and register license for Kanban component

--- src/providers/QueryClientProvider.tsx
+++ src/providers/QueryClientProvider.tsx
@@ -4,6 +4,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
 
+import { registerLicense } from "@syncfusion/ej2-base";
+
+registerLicense(
+  "Ngo9BigBOggjHTQxAR8/V1NNaF5cXmBCe0x3WmFZfVtgd19HaVZSQmYuP1ZhSXxWdkFhXX9dcX1WQGhUVEB9XUs=",
+);
+
 function QueryClientProviderWrapper({
   children,
 }: Readonly<{ children: ReactNode }>) {
