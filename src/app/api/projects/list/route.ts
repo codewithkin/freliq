5526395be9622b34e4c701a14d959f458d0cf2f8
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-24 12:36:43 +0200

    feat: implement GET endpoint to retrieve user projects from the database

---
+++ src/app/api/projects/list/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { prisma } from "@/prisma";
+
+export async function GET() {
+  try {
+    // Get the user's session
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    });
+
+    // Get the user's id
+    const id = session?.user?.id;
+
+    // Get all of the user's projects
+    const projects = await prisma.project.findMany({
+      where: {
+        ownerId: id,
+      },
+    });
+
+    return NextResponse.json(projects);
+  } catch (e) {
+    console.log("An error occured while getting projects: ", e);
+
+    return NextResponse.json(
+      {
+        message: "An error occured while getting projects",
+      },
+      { status: 400 },
+    );
+  }
+}
