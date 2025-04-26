a692e6db9d8564ec48d7aa97833fc3b54b2a2c16
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-26 15:22:48 +0200

    [REMOVE]: Footer for Projects Line Chart

--- src/app/projects/components/charts/ProjectsLineChart.tsx
+++ src/app/projects/components/charts/ProjectsLineChart.tsx
@@ -105,14 +105,6 @@ export function ProjectsLineChart({ projects }: ProjectsLineChartProps) {
           </LineChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter className="flex-col items-start gap-2 text-sm">
-        <div className="flex gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="leading-none text-muted-foreground">
-          Showing total projects for the last months
-        </div>
-      </CardFooter>
     </Card>
   );
 }
