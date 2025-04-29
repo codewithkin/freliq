6a719b24a516b3c2de9ae13ba40d28c724aa34a2
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-29 09:47:17 +0200

    ADD: Integrate Zustand for state management in new project data store

---
+++ src/stores/useNewProjectData.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+
+type Actions = {
+    setTitle: (title: string) => void,
+    setDescription: (description: string) => void,
+    setDeadline: (deadline: Date | string) => void,
+    setImage: (image: string) => void,
+    setOwnerId: (ownerId: string) => void,
+    setTasks: (tasks: any) => void
+}
+
+type State = {
+    data: {
+        title?: string,
+        description?: string,
+        deadline?: Date | string,
+        image?: string,
+        ownerId?: string,
+        tasks?: any
+    }
+    highestCompletedStep: number
+}
+
+export const useNewProjectData = create<State & Actions>((set) => ({
+    data: {},
+    highestCompletedStep: 0,
+    setTitle: (title: string) => set((state) => ({ data: { ...state.data, title } })),
+    setDescription: (description: string) => set((state) => ({ data: { ...state.data, description } })),
+    setDeadline: (deadline: Date | string) => set((state) => ({ data: { ...state.data, deadline } })),
+    setImage: (image: string) => set((state) => ({ data: { ...state.data, image } })),
+    setOwnerId: (ownerId: string) => set((state) => ({ data: { ...state.data, ownerId } })),
+    setTasks: (tasks: any) => set((state) => ({ data: { ...state.data, tasks } }))
+}));
+
