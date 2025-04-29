228d28fad18a964c4ac4e591aa45e3eb9235f302
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-29 09:53:51 +0200

    FORMAT: Refactor type definitions for clarity in useNewProjectData store

--- src/stores/useNewProjectData.ts
+++ src/stores/useNewProjectData.ts
@@ -1,34 +1,39 @@
 import { create } from "zustand";
 
 type Actions = {
-    setTitle: (title: string) => void,
-    setDescription: (description: string) => void,
-    setDeadline: (deadline: Date | string) => void,
-    setImage: (image: string) => void,
-    setOwnerId: (ownerId: string) => void,
-    setTasks: (tasks: any) => void
-}
+  setTitle: (title: string) => void;
+  setDescription: (description: string) => void;
+  setDeadline: (deadline: Date | string) => void;
+  setImage: (image: string) => void;
+  setOwnerId: (ownerId: string) => void;
+  setTasks: (tasks: any) => void;
+};
 
 type State = {
-    data: {
-        title?: string,
-        description?: string,
-        deadline?: Date | string,
-        image?: string,
-        ownerId?: string,
-        tasks?: any
-    }
-    highestCompletedStep: number
-}
+  data: {
+    title?: string;
+    description?: string;
+    deadline?: Date | string;
+    image?: string;
+    ownerId?: string;
+    tasks?: any;
+  };
+  highestCompletedStep: number;
+};
 
 export const useNewProjectData = create<State & Actions>((set) => ({
-    data: {},
-    highestCompletedStep: 0,
-    setTitle: (title: string) => set((state) => ({ data: { ...state.data, title } })),
-    setDescription: (description: string) => set((state) => ({ data: { ...state.data, description } })),
-    setDeadline: (deadline: Date | string) => set((state) => ({ data: { ...state.data, deadline } })),
-    setImage: (image: string) => set((state) => ({ data: { ...state.data, image } })),
-    setOwnerId: (ownerId: string) => set((state) => ({ data: { ...state.data, ownerId } })),
-    setTasks: (tasks: any) => set((state) => ({ data: { ...state.data, tasks } }))
+  data: {},
+  highestCompletedStep: 0,
+  setTitle: (title: string) =>
+    set((state) => ({ data: { ...state.data, title } })),
+  setDescription: (description: string) =>
+    set((state) => ({ data: { ...state.data, description } })),
+  setDeadline: (deadline: Date | string) =>
+    set((state) => ({ data: { ...state.data, deadline } })),
+  setImage: (image: string) =>
+    set((state) => ({ data: { ...state.data, image } })),
+  setOwnerId: (ownerId: string) =>
+    set((state) => ({ data: { ...state.data, ownerId } })),
+  setTasks: (tasks: any) =>
+    set((state) => ({ data: { ...state.data, tasks } })),
 }));
-
