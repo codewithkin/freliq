d4afd8d9a07b3c82c6462baa223a787dc30c2a19
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-29 12:26:53 +0200

    Add project creation functionality with image upload and success notification

--- src/app/project/new/second/page.tsx
+++ src/app/project/new/second/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import FlowContainer from "../components/FlowContainer";
 import { useDropzone } from "react-dropzone";
+import { toast } from "sonner";
+import { useNewProjectData } from "@/stores/useNewProjectData";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Loader, Loader2 } from "lucide-react";
 
 export default function NewProjectPage() {
   const router = useRouter();
+
+  const [created, setCreated] = useState<boolean>(false);
 
   // Get the user's data
   const { data: user, isLoading } = useQuery({
@@ -16,6 +23,26 @@ export default function NewProjectPage() {
       const res = await axios.get("/api/user");
 
       return res.data.fullUser;
+    },
+  });
+
+  const projectData = useNewProjectData((state) => state.data);
+
+  // Create project mutation
+  const { mutate: createProject, isPending: creatingProject } = useMutation({
+    mutationKey: ["createProject"],
+    mutationFn: async () => {
+      const res = await axios.post(`/api/project`, { data: projectData });
+
+      return res.data;
+    },
+    onSuccess: () => {
+      toast.success("Project created successfully");
+
+      setCreated(true);
+    },
+    onError: () => {
+      toast.error("Could not create project, try again later");
     },
   });
 
@@ -31,6 +58,7 @@ export default function NewProjectPage() {
     <FlowContainer
       title="Upload Project Image"
       description="Upload a memorable image of the project (maybe a banner, or an icon)"
+      disabled={creatingProject || !created}
     >
       <article className="space-y-6 w-full">
         <article className="p-4 border-2 border-dashed border-gray-300 rounded-lg">
@@ -41,6 +69,20 @@ export default function NewProjectPage() {
             </h2>
           </article>
         </article>
+
+        {!created && (
+          <Button
+            disabled={creatingProject}
+            onClick={() => {
+              createProject();
+            }}
+          >
+            {creatingProject && <Loader2 className="animate-spin" />}{" "}
+            {creatingProject
+              ? "Creating project..."
+              : "Upload and create project"}
+          </Button>
+        )}
       </article>
     </FlowContainer>
   );
