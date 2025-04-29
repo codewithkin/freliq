a06f03f43c10ee439c30056d50098456164bd48e
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-29 09:54:03 +0200

    ADD: Display current step and progress in FlowContainer

--- src/app/project/new/components/FlowContainer.tsx
+++ src/app/project/new/components/FlowContainer.tsx
@@ -8,6 +8,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+import { useNewProjectData } from "@/stores/useNewProjectData";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
@@ -32,13 +34,39 @@ function FlowContainer({
   const second = "/project/new/second";
   const third = "/project/new/third";
 
+  // Get the steps data
+  const highestCompletedStep = useNewProjectData(
+    (state) => state.highestCompletedStep,
+  );
+
   return (
     <section className="w-screen h-screen flex flex-col justify-center items-center bg-gradient-to-tr from-purple-600 to-sky-400 p-4">
       <Card>
         <CardContent>
-          <CardHeader className="px-0 pb-8">
-            <CardTitle className="text-2xl font-semibold">{title}</CardTitle>
-            <CardDescription>{description}</CardDescription>
+          <CardHeader className="px-0 pb-8 w-full">
+            <article className="my-4 flex flex-col gap-2">
+              <p className="text-sm text-muted-foreground">
+                Step{" "}
+                {highestCompletedStep > 0
+                  ? highestCompletedStep
+                  : highestCompletedStep + 1}{" "}
+                of 4
+              </p>
+              <Progress
+                max={100}
+                className="h-6 my-0"
+                value={
+                  highestCompletedStep > 0
+                    ? highestCompletedStep * 25
+                    : (highestCompletedStep + 1) & 25
+                }
+              />
+            </article>
+
+            <article>
+              <CardTitle className="text-2xl font-semibold">{title}</CardTitle>
+              <CardDescription>{description}</CardDescription>
+            </article>
           </CardHeader>
           {children}
         </CardContent>
